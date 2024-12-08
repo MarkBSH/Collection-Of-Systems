205a7d51a7f635757cab2f1eca2b860cb11dde4e
MarkBSH <145546641+MarkBSH@users.noreply.github.com>
2024-12-08 19:48:07 +0100

    Refactor SideScrollingMovement script to improve organization and readability by grouping related methods and variables

--- Assets/Systems/Basic + Different Systems/Movement/2D/Side-Scrolling/Scripts/SideScrollingMovement.cs
+++ Assets/Systems/Basic + Different Systems/Movement/2D/Side-Scrolling/Scripts/SideScrollingMovement.cs
@@ -3,19 +3,12 @@ using UnityEngine.InputSystem;
 
 public class SideScrollingMovement : MonoBehaviour
 {
-    public float m_speed = 5f;
-    public Vector2 m_move;
-    public float m_jumpForce = 10f;
-    public Transform m_groundCheck;
-    public LayerMask m_groundLayer;
-
-    private Rigidbody m_rb;
-    private bool isGrounded;
-    private float m_groundCheckRadius = 0.2f;
+    #region Unity Methods
 
     private void Start()
     {
-        m_rb = GetComponent<Rigidbody>();
+        GetComponents();
+        GetGroundCheck();
     }
 
     private void Update()
@@ -28,9 +21,27 @@ public class SideScrollingMovement : MonoBehaviour
         MoveCharacter();
     }
 
-    private void CheckGroundStatus()
+    #endregion
+
+    #region Components
+
+    private Rigidbody m_rb;
+
+    private void GetComponents()
     {
-        isGrounded = Physics.OverlapSphere(m_groundCheck.position, m_groundCheckRadius, m_groundLayer).Length > 0;
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    #endregion
+
+    #region Movement
+
+    public float m_speed = 5f;
+    private Vector2 m_move;
+
+    public void OnMove(InputAction.CallbackContext _context)
+    {
+        m_move = _context.ReadValue<Vector2>();
     }
 
     private void MoveCharacter()
@@ -38,28 +49,53 @@ public class SideScrollingMovement : MonoBehaviour
         m_rb.linearVelocity = new Vector2(m_move.x * m_speed, m_rb.linearVelocity.y);
     }
 
-    public void OnMove(InputAction.CallbackContext _context)
+    #endregion
+
+    #region Jumping
+
+    public float m_jumpForce = 10f;
+    private Transform m_groundCheck;
+    public LayerMask m_groundLayer;
+    private bool isGrounded;
+    private float m_groundCheckRadius = 0.2f;
+
+    private void GetGroundCheck()
     {
-        m_move = _context.ReadValue<Vector2>();
+        m_groundCheck = transform.Find("GroundCheck");
     }
 
     public void OnJump(InputAction.CallbackContext _context)
     {
-        if (_context.performed && isGrounded)
+        if (_context.performed && isGrounded && !isCrouching)
         {
             m_rb.linearVelocity = new Vector2(m_rb.linearVelocity.x, m_jumpForce);
         }
     }
 
+    private void CheckGroundStatus()
+    {
+        isGrounded = Physics.CheckSphere(m_groundCheck.position, m_groundCheckRadius, m_groundLayer);
+    }
+
+    #endregion
+
+    #region Crouching
+
+    private bool isCrouching = false;
+
     public void OnCrouch(InputAction.CallbackContext _context)
     {
         if (_context.performed && isGrounded)
         {
+            isCrouching = true;
             Debug.Log("Crouching");
         }
         else if (_context.canceled && isGrounded)
         {
+            isCrouching = false;
             Debug.Log("Standing up");
         }
     }
+
+    #endregion
 }
