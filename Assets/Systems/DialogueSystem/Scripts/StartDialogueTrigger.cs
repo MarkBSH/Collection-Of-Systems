4f177ab69406276bb602bf6be8ec5fedbff02e2c
MarkBSH <145546641+MarkBSH@users.noreply.github.com>
2024-12-08 22:25:56 +0100

    Refactor StartDialogueTrigger for improved organization and readability, including method extraction and enhanced debugging

--- Assets/Systems/DialogueSystem/Scripts/StartDialogueTrigger.cs
+++ Assets/Systems/DialogueSystem/Scripts/StartDialogueTrigger.cs
@@ -2,21 +2,49 @@ using UnityEngine;
 
 public class StartDialogueTrigger : MonoBehaviour
 {
-    [SerializeField] private int m_DialogueIndex;
+    #region Unity Methods
 
     private void OnTriggerEnter(Collider _other)
     {
         if (_other.CompareTag("Player"))
         {
-            var dialogueManager = DialogueManager.Instance;
-            if (dialogueManager != null)
-            {
-                dialogueManager.StartDialogue(m_DialogueIndex);
-            }
-            else
-            {
-                Debug.LogWarning("DialogueManager instance is null.");
-            }
+            StartDialogue();
         }
     }
+
+    #endregion
+
+    #region Components
+
+
+
+    #endregion
+
+    #region DialogueStarter
+
+    public int m_DialogueIndex;
+
+    public void StartDialogue()
+    {
+        var _DialogueManager = DialogueManager.Instance;
+
+        if (_DialogueManager == null)
+        {
+            DebugWarning("DialogueManager instance is null.");
+            return;
+        }
+
+        _DialogueManager.StartDialogue(m_DialogueIndex);
+    }
+
+    #endregion
+
+    #region Debugging
+
+    private void DebugWarning(string _warning)
+    {
+        Debug.LogWarning("Warning: " + _warning);
+    }
+
+    #endregion
 }
