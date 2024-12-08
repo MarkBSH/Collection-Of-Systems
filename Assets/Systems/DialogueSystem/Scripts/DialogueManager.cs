b0085fc5865f7394bc78b52617d77b98cef2120c
MarkBSH <145546641+MarkBSH@users.noreply.github.com>
2024-12-08 22:20:23 +0100

    Remove BasicPlayerMovement script and refactor DialogueManager for improved input handling and component retrieval

--- Assets/Systems/DialogueSystem/Scripts/DialogueManager.cs
+++ Assets/Systems/DialogueSystem/Scripts/DialogueManager.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.InputSystem;
 
 public class DialogueManager : MonoBehaviour
 {
+    #region Singleton
+
     private static DialogueManager m_Instance;
     public static DialogueManager Instance
     {
@@ -25,46 +28,81 @@ public class DialogueManager : MonoBehaviour
         }
     }
 
-    [SerializeField] private List<Dialogue> m_Dialogues = new();
-    private GameObject m_DialogueBox;
-    private TextMeshProUGUI m_TextArea;
-    private TextMeshProUGUI m_NameArea;
-    private Image m_SpriteArea;
-    private AudioSource m_AudioManager;
-    private bool m_IsWaitingForInput;
-    private float m_normalPlayerSpeed = 5f;
+    #endregion
+
+    #region Unity Methods
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
-        InitializeDialogueComponents();
-        m_DialogueBox.SetActive(false);
+        GetDialogueComponents();
+        GetAudioManager();
+        DisableDialogueBox();
     }
 
-    private void InitializeDialogueComponents()
-    {
-        m_DialogueBox = GameObject.Find("DialogueBox");
-        m_TextArea = GameObject.Find("TextArea").GetComponent<TextMeshProUGUI>();
-        m_NameArea = GameObject.Find("NameArea").GetComponent<TextMeshProUGUI>();
-        m_SpriteArea = GameObject.Find("SpriteArea").GetComponent<Image>();
-        m_AudioManager = GetComponent<AudioSource>();
-    }
+    #endregion
 
-    private void Update()
+    #region Components
+
+
+
+    #endregion
+
+    #region DialogueInput
+
+    private bool m_IsWaitingForInput;
+
+    public void NextSentence(InputAction.CallbackContext _context)
     {
-        if (m_IsWaitingForInput && Input.GetKeyDown(KeyCode.Space))
+        if (_context.performed)
         {
             m_IsWaitingForInput = false;
         }
     }
 
+    #endregion
+
+    #region Dialogue
+
+    [SerializeField] private List<Dialogue> m_Dialogues = new();
+    private GameObject m_DialogueBox;
+    private TextMeshProUGUI m_TextArea;
+    private TextMeshProUGUI m_NameArea;
+    private Image m_SpriteArea;
+
+    private void GetDialogueComponents()
+    {
+        m_DialogueBox = GameObject.Find("DialogueBox");
+        if (m_DialogueBox == null)
+        {
+            DebugWarning("No DialogueBox found. Please add a DialogueBox to the scene");
+        }
+        m_TextArea = GameObject.Find("TextArea").GetComponent<TextMeshProUGUI>();
+        if (m_TextArea == null)
+        {
+            DebugWarning("No TextArea found. Please add a TextArea to the scene");
+        }
+        m_NameArea = GameObject.Find("NameArea").GetComponent<TextMeshProUGUI>();
+        if (m_NameArea == null)
+        {
+            DebugWarning("No NameArea found. Please add a NameArea to the scene");
+        }
+        m_SpriteArea = GameObject.Find("SpriteArea").GetComponent<Image>();
+        if (m_SpriteArea == null)
+        {
+            DebugWarning("No SpriteArea found. Please add a SpriteArea to the scene");
+        }
+    }
+
+    private void DisableDialogueBox()
+    {
+        m_DialogueBox.SetActive(false);
+    }
+
     public void StartDialogue(int _dialogueIndex)
     {
-        m_normalPlayerSpeed = BasicPlayerMovement.m_Speed;
-        BasicPlayerMovement.m_Speed = 0;
-
+        // Stop the player from moving
         m_DialogueBox.SetActive(true);
-        Debug.Log("Starting dialogue");
         StartCoroutine(DialogueCoroutine(_dialogueIndex));
     }
 
@@ -81,24 +119,50 @@ public class DialogueManager : MonoBehaviour
             foreach (char letter in dialogue.m_Sentence[i].ToCharArray())
             {
                 m_TextArea.text += letter;
-                yield return new WaitForSeconds(0.05f); // Adjust the speed of the text display here
+                yield return new WaitForSeconds(0.05f);
             }
 
             m_IsWaitingForInput = true;
             yield return new WaitUntil(() => !m_IsWaitingForInput);
-            m_AudioManager.Stop(); // Stop the audio after waiting for input
+            m_AudioManager.Stop();
         }
-        Debug.Log("Ending dialogue");
-
         EndDialogue();
     }
 
     private void EndDialogue()
     {
-        BasicPlayerMovement.m_Speed = m_normalPlayerSpeed;
-        m_DialogueBox.SetActive(false);
+        // Reset the player speed
+        DisableDialogueBox();
     }
+
+    #endregion
+
+    #region Audio
+
+    private AudioSource m_AudioManager;
+
+    private void GetAudioManager()
+    {
+        m_AudioManager = GetComponent<AudioSource>();
+        if (m_AudioManager == null)
+        {
+            DebugWarning("No AudioSource component found. Please add an AudioSource component to the GameObject");
+        }
+    }
+
+    #endregion
+
+    #region Debugging
+
+    private void DebugWarning(string _warning)
+    {
+        Debug.LogWarning("Warning: " + _warning);
+    }
+
+    #endregion
 }
+
+#region Dialogue Class
 
 [System.Serializable]
 public class Dialogue
@@ -109,3 +173,5 @@ public class Dialogue
     public List<Sprite> m_Sprite;
     public List<AudioClip> m_AudioClip;
 }
+
+#endregion
