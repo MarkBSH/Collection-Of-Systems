edef4b9f854eb2586fc9b7c07d05ecb980c16020
MarkBSH <145546641+MarkBSH@users.noreply.github.com>
2024-12-08 22:33:16 +0100

    Refactor Item class for improved organization and readability, including region definitions for item details and methods

--- Assets/Systems/InventorySystem/Scripts/Item.cs
+++ Assets/Systems/InventorySystem/Scripts/Item.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+#region Item Enums
+
 public enum ItemType
 {
     Weapon,
@@ -8,15 +10,23 @@ public enum ItemType
     Placeable
 }
 
+#endregion
+
 [CreateAssetMenu(fileName = "Item", menuName = "Scriptable Objects/Item")]
 public class Item : ScriptableObject
 {
+    #region Private Item Details
+
     [SerializeField] private int m_ItemID;
     [SerializeField] private string m_ItemName;
     [SerializeField] private Sprite m_ItemSprite;
     [SerializeField] private int m_ItemValue;
     [SerializeField] private ItemType m_ItemType;
     [SerializeField] private string m_ItemDescription;
+
+    #endregion
+
+    #region Public Item Details
 
     public int ItemID => m_ItemID;
     public string ItemName => m_ItemName;
@@ -25,8 +35,14 @@ public class Item : ScriptableObject
     public ItemType ItemType => m_ItemType;
     public string ItemDescription => m_ItemDescription;
 
+    #endregion
+
+    #region Item Methods
+
     public string GetItemDetails()
     {
         return $"ID: {m_ItemID}\nName: {m_ItemName}\nDescription: {m_ItemDescription}\nValue: {m_ItemValue}\nType: {m_ItemType}";
     }
+
+    #endregion
 }
