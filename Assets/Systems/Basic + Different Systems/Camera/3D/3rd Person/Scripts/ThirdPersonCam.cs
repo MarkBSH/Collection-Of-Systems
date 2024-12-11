152dc9e012a809989c7d2c4fb2fa01a94d7c9a54
MarkBSH <145546641+MarkBSH@users.noreply.github.com>
2024-12-11 11:20:27 +0100

    Implement camera movement and initialization in ThirdPersonCam; add player and camera parent retrieval methods

--- Assets/Systems/Basic + Different Systems/Camera/3D/3rd Person/Scripts/ThirdPersonCam.cs
+++ Assets/Systems/Basic + Different Systems/Camera/3D/3rd Person/Scripts/ThirdPersonCam.cs
@@ -27,19 +27,64 @@ public class ThirdPersonCam : MonoBehaviour
 
     #region Unity Methods
 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        GetPlayer();
+        GetCameraParent();
+    }
 
+    private void Update()
+    {
+        MoveCamera();
+    }
 
     #endregion
 
     #region Components
 
+    private GameObject m_Player;
+    private GameObject m_CameraParent;
 
+    private void GetPlayer()
+    {
+        m_Player = transform.parent.parent.gameObject;
+        if (m_Player == null)
+        {
+            DebugWarning("Player not found. Please add a GameObject with the tag 'Player'");
+        }
+    }
+
+    private void GetCameraParent()
+    {
+        m_CameraParent = transform.parent.gameObject;
+        if (m_CameraParent == null)
+        {
+            DebugWarning("CameraParent not found");
+        }
+    }
 
     #endregion
 
     #region Camera Movement
 
+    [SerializeField] private float m_MouseSensitivity;
+    [SerializeField] private float m_MaxCamTop;
+    [SerializeField] private float m_MaxCamBottom;
+    private float m_AngleX;
+    private float m_AngleY;
 
+    private void MoveCamera()
+    {
+        float _mouseX = Input.GetAxis("Mouse X") * m_MouseSensitivity * Time.deltaTime;
+        float _mouseY = Input.GetAxis("Mouse Y") * m_MouseSensitivity * Time.deltaTime;
+
+        m_AngleX += _mouseX;
+        m_AngleY -= _mouseY;
+        m_AngleY = Mathf.Clamp(m_AngleY, m_MaxCamBottom, m_MaxCamTop);
+
+        m_CameraParent.transform.localRotation = Quaternion.Euler(m_AngleY, m_AngleX, 0f);
+    }
 
     #endregion
 
